51286e11d05e3a0486a38076fc47f4a99f2b9db9
Angoliuk <gamongara@gmail.com>
2024-10-14 18:29:07 +0300

    deps fix

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,16 +8,3 @@ endif()
 
 find_package(fmt CONFIG REQUIRED)
 target_link_libraries(${PROJECT_NAME} PUBLIC fmt::fmt-header-only)
-
-if(BUILD_WASM)
-  set(glad_DIR "./build/web/build/Debug/generators")
-else()
-  set(glad_DIR "./build/app/build/Debug/generators")
-endif()
-
-find_package(glad CONFIG REQUIRED)
-target_link_libraries(${PROJECT_NAME} PUBLIC glad::glad)
-
-find_package(OpenGL REQUIRED)
-target_link_libraries(${PROJECT_NAME} PUBLIC ${OPENGL_LIBRARIES})
-target_include_directories(${PROJECT_NAME} PUBLIC ${OPENGL_INCLUDE_DIR})
