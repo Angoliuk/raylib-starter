ef7c708314738bb20d7fc283a81f553c4dd4dc66
Angoliuk <gamongara@gmail.com>
2024-10-14 18:22:26 +0300

    removed dotnet

--- cmake/unix/compiler.cmake
+++ cmake/unix/compiler.cmake
@@ -3,14 +3,3 @@ set(CMAKE_CXX_COMPILER "clang++")
 add_compile_options(-fms-extensions)
 
 ###===================================================================================##
-
-if(BUILD_DOTNET)
-  add_custom_command(TARGET ${PROJECT_NAME}
-    POST_BUILD
-    COMMAND dotnet publish Wind.csproj
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/engine
-    COMMENT "Running dotnet publish"
-  )
-endif()
-
-###===================================================================================##
--- cmake/wasm/compiler.cmake
+++ cmake/wasm/compiler.cmake
@@ -3,15 +3,6 @@ set(CMAKE_CXX_COMPILER "em++")
 
 ###===================================================================================##
 
-if(BUILD_DOTNET)
-  add_custom_command(TARGET ${PROJECT_NAME}
-    POST_BUILD
-    COMMAND dotnet publish Wind.Web.csproj
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/engine
-    COMMENT "Running dotnet publish"
-  )
-endif()
-
 set_target_properties(${PROJECT_NAME} PROPERTIES
   OUTPUT_NAME "wind"
   SUFFIX ".wasm"
